95634a5b0510b4c0c07562d292f9a194288234c8
William Guinaudie <william.guinaudie@gmail.com>
2023-02-12 00:33:46 +0100

    release v1.09: added leboncoin route+ implemented in the front

--- frontend/src/hooks/search/useSearch.ts
+++ frontend/src/hooks/search/useSearch.ts
@@ -1,6 +1,9 @@
 import { useRouter } from "next/router";
 
 const useSearch = () => {
+  if (typeof window === "undefined") {
+    return { apiUrl: "", searchTerm: "", encodedSearchTerm: "" };
+  }
   const router = useRouter();
   const searchTerm = (router.query?.query as string) || "";
   const apiUrl = (window && `${window.location.origin}/api`) || "";
