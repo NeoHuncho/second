41058ed6e5b949aa537519747197413b97ec7dc5
William Guinaudie <william.guinaudie@gmail.com>
2023-02-13 14:03:17 +0100

    release v1.1: removed backend . one main "second" file now

--- backend-second/tests/integration/api.test.ts
+++
@@ -1,8 +0,0 @@
-import { api } from "@serverless/cloud";
-
-test("should return users", async () => {
-  const { body } = await api.get("/users").invoke();
-
-  expect(body).toHaveProperty("users");
-  expect(body.users.length).toBeGreaterThan(0);
-});
