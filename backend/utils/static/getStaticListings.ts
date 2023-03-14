816e6d8f63e9f0215457e792cc24fd1b1cad2a7f
William Guinaudie <william.guinaudie@gmail.com>
2023-03-14 21:51:18 +0100

    release v0.4592: added cat suggestion logic to getShop backend

--- backend/utils/static/getStaticListings.ts
+++ backend/utils/static/getStaticListings.ts
@@ -2,7 +2,7 @@ import { readFile } from "fs/promises";
 import type { ShopName } from "../../../common/types/types";
 
 const getStaticListings = async (shopName: ShopName) => {
-  return await readFile(`public/static/shops/${shopName}.txt`, {
+  return await readFile(`backend/static/shops/${shopName}.txt`, {
     encoding: "utf8",
   });
 };
