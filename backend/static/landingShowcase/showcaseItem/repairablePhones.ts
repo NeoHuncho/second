598d1abc3dad50aca98736a801602228f343540e
William Guinaudie <william.guinaudie@gmail.com>
2023-06-01 23:03:17 +0200

    release v0.397: added more showcase phones

--- backend/static/landingShowcase/showcaseItem/repairablePhones.ts
+++ backend/static/landingShowcase/showcaseItem/repairablePhones.ts
@@ -3,11 +3,18 @@ import type {
   ListingChildren,
 } from "../../../../src/types/types";
 import iphone14img from "../../../assets/products/phones/apple_iphone14.png";
-import samsungA52 from "../../../assets/products/phones/samsung_a52.png";
-import crosscallCore from "../../../assets/products/phones/crosscall_core.png";
+import coreZ5 from "../../../assets/products/phones/CORE-Z5.webp";
+import coreX5 from "../../../assets/products/phones/CORE-X5.webp";
+import coreM5 from "../../../assets/products/phones/CORE-M5.webp";
+import coreX4 from "../../../assets/products/phones/CORE-X4.webp";
+import A14 from "../../../assets/products/phones/a14.webp";
+import A54 from "../../../assets/products/phones/a54.webp";
+import A53 from "../../../assets/products/phones/a53.webp";
+import A34 from "../../../assets/products/phones/a34.webp";
+import A23 from "../../../assets/products/phones/a23.webp";
+
 import fairphone3 from "../../../assets/products/phones/fairphone_3.jpeg";
 import fairphone4 from "../../../assets/products/phones/fairphone_4.png";
-
 import redmiNote11 from "../../../assets/products/phones/redmi_note11.png";
 import vivoV52 from "../../../assets/products/phones/vivo_y52.png";
 import motorolaE13 from "../../../assets/products/phones/motorola_e13.png";
@@ -43,18 +50,94 @@ const repairablePhones: LandingListing[] = [
     ],
   },
   {
-    title: "CROSSCALL Core M5/Z5/X5",
-    repairScore: 8.8,
-    image: crosscallCore,
+    title: "CROSSCALL Core 5",
+    repairScore: 9.1,
+    image: coreZ5,
     url: "/search?query=crosscall+core+m5",
     type: "Showcase",
+    children: [
+      {
+        title: "CROSSCALL Core-Z5",
+        repairScore: 9.1,
+        image: coreZ5,
+        url: "/search?query=crosscall+core+z5",
+        type: "Showcase",
+        priceRange: 3,
+      },
+      {
+        title: "CROSSCALL Core-X5",
+        repairScore: 9.1,
+        image: coreX5,
+        url: "/search?query=crosscall+core+x5",
+        type: "Showcase",
+        priceRange: 2,
+      },
+      {
+        title: "CROSSCALL Core-M5",
+        repairScore: 8.8,
+        image: coreM5,
+        url: "/search?query=crosscall+core+m5",
+        type: "Showcase",
+        priceRange: 1,
+      },
+      {
+        title: "CROSSCALL Core-X4",
+        repairScore: 8.8,
+        image: coreX4,
+        url: "/search?query=crosscall+core+x4",
+        type: "Showcase",
+        priceRange: 1,
+      },
+    ],
   },
   {
-    title: "SAMSUNG Galaxy A52",
-    repairScore: 8.2,
-    image: samsungA52,
+    title: "Samsung Galaxy A",
+    repairScore: 8.8,
+    image: A54,
     url: "/search?query=galaxy+a52",
     type: "Showcase",
+    children: [
+      {
+        title: "Samsung Galaxy A14",
+        repairScore: 8.8,
+        image: A14,
+        url: "/search?query=galaxy+a14",
+        type: "Showcase",
+        priceRange: 1,
+      },
+      {
+        title: "Samsung Galaxy A54",
+        repairScore: 8.4,
+        image: A54,
+        url: "/search?query=galaxy+a54",
+        type: "Showcase",
+        priceRange: 2,
+      },
+      {
+        title: "Samsung Galaxy A53",
+        repairScore: 8.2,
+        image: A53,
+        url: "/search?query=galaxy+a53",
+        type: "Showcase",
+        priceRange: 2,
+      },
+      {
+        title: "Samsung Galaxy A34",
+        repairScore: 8.3,
+        image: A34,
+        url: "/search?query=galaxy+a34",
+        type: "Showcase",
+        priceRange: 2,
+      },
+      {
+        title: "Samsung Galaxy A23",
+        repairScore: 8.1,
+        image: A23,
+        url: "/search?query=galaxy+a23",
+        type: "Showcase",
+        priceRange: 1,
+      },
+    ],
   },
 
   {
